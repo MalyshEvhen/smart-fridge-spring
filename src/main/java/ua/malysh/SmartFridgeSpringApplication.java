06f9c4cf6cb2a3c9d917e7896ad22c53189e52e3
Evhen <malysh.evgeniy@gmail.com>
2023-04-03 08:20:50 +0300

    Initial commit

---
+++ src/main/java/ua/malysh/SmartFridgeSpringApplication.java
@@ -0,0 +1,13 @@
+package ua.malysh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFridgeSpringApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFridgeSpringApplication.class, args);
+    }
+    
+}
